b4dac5c25673b19256392e3411ff20b1ce0d0e7d
vanlach <evantarlach@Evans-MacBook-Pro-3.local>
2020-12-18 11:33:55 -0600

    Perish Song and Spikes can leave a Pokémon with 0 HP and not faint
    https://github.com/pret/pokecrystal/blob/master/docs/bugs_and_glitches.md#multi-player-battle-engine

--- engine/battle/core.asm
+++ engine/battle/core.asm
@@ -249,7 +249,20 @@ HandleBetweenTurnEffects:
 	call LoadTilemapToTempTilemap
 	jp HandleEncore
 
+HasAnyoneFainted:
+	call HasPlayerFainted
+	jp nz, HasEnemyFainted
+	ret
+
 CheckFaint_PlayerThenEnemy:
+.faint_loop
+	call .Function
+	ret c
+	call HasAnyoneFainted
+	ret nz
+	jr .faint_loop
+
+.Function:
 	call HasPlayerFainted
 	jr nz, .PlayerNotFainted
 	call HandlePlayerMonFaint
@@ -274,6 +287,14 @@ CheckFaint_PlayerThenEnemy:
 	ret
 
 CheckFaint_EnemyThenPlayer:
+.faint_loop
+	call .Function
+	ret c
+	call HasAnyoneFainted
+	ret nz
+	jr .faint_loop
+
+.Function:
 	call HasEnemyFainted
 	jr nz, .EnemyNotFainted
 	call HandleEnemyMonFaint
