e0d9d7da54a3e553c688b662efaa9ae3810409e4
vanlach <evantarlach@Evans-MacBook-Pro-3.local>
2020-12-18 19:59:22 -0600

    Revert "Unique colors for each Pokéball when thrown"
    This reverts commit 18b0a7ffcb5fe10f489c5f997ac569af81292932.

--- engine/battle_anims/functions.asm
+++ engine/battle_anims/functions.asm
@@ -429,10 +429,26 @@ BattleAnimFunction_PokeBallBlocked:
 	ret
 
 GetBallAnimPal:
+	ld hl, BallColors
+	ld a, [wCurItem]
+	ld e, a
+.IsInArray:
+	ld a, [hli]
+	cp -1
+	jr z, .load
+	cp e
+	jr z, .load
+	inc hl
+	jr .IsInArray
+
+.load
+	ld a, [hl]
 	ld hl, BATTLEANIMSTRUCT_PALETTE
 	add hl, bc
-	ld [hl], PAL_BATTLE_OB_RED
+	ld [hl], a
 	ret
+
+INCLUDE "data/battle_anims/ball_colors.asm"
 
 BattleAnimFunction_Ember:
 	call BattleAnim_AnonJumptable
